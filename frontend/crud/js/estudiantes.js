d5f1c90c364a0a3df51f2a2c5125631c1cafd92b
camilopr11 <camilop.rivas@gmail.com>
2020-02-13 06:06:31 -0500

    Crud

--- frontend/crud/js/estudiantes.js
+++ frontend/crud/js/estudiantes.js
@@ -1,4 +1,31 @@
 console.log("-------------------JS CRUD ESTUDIANTES---------")
 
-let id = document.getElementById("id")
-console.log("Mi id:", id)
+let estudiantes = []
+
+function obtenerValores() {
+    let id = document.getElementById("id").value
+    let nombre = document.getElementById("nombre").value
+    let apellido = document.getElementById("apellido").value
+    let edad = document.getElementById("edad").value
+    let correo = document.getElementById("correo").value
+    let carrera = document.getElementById("carrera").value
+
+    //Primera forma
+    let miEstudiante = {}
+    miEstudiante.id = id
+    miEstudiante.nombre = nombre
+    console.log(miEstudiante)
+
+    //Segunda forma
+    let miEstudiante2 = { id, nombre, apellido, edad, correo, carrera }
+    console.log(miEstudiante2)
+    return miEstudiante2
+
+}
+
+function crearEstudiante() {
+    let estudiante = obtenerValores()
+    estudiantes.push(estudiante)
+    console.log(estudiantes)
+
+}
